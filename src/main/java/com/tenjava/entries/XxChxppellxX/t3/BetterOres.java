b9c899c1224c27ffe0950d0066f588b3b01c2a30
XxChxppellxX <ka>
2014-07-12 15:30:45 +0100

    Started adding stuff for testing.

---
+++ src/main/java/com/tenjava/entries/XxChxppellxX/t3/BetterOres.java
@@ -0,0 +1,16 @@
+package com.tenjava.entries.XxChxppellxX.t3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterOres extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
+
+
